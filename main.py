61d7d19b34e2b7d6c34b69041b21685fecce6e55
Shivam <47765144+shivamCode0@users.noreply.github.com>
2024-01-14 09:20:44 -0500

    changes

--- main.py
+++ main.py
@@ -53,20 +53,20 @@ class Circle:
 circles = [
     Circle(mass=1, radius=10 * math.sqrt(1), x=width / 6, y=height / 2),
     Circle(mass=2, radius=10 * math.sqrt(2), x=2 * width / 6, y=height / 2),
-    Circle(mass=3, radius=10 * math.sqrt(3), x=3 * width / 6, y=wheight / 2),
+    Circle(mass=3, radius=10 * math.sqrt(3), x=3 * width / 6, y=height / 2),
     Circle(mass=4, radius=10 * math.sqrt(4), x=4 * width / 6, y=height / 2),
     Circle(mass=5, radius=10 * math.sqrt(5), x=5 * width / 6, y=height / 2),
-    # Circle(mass=50, radius=10 * math.sqrt(5), x=width / 2, y=height / 2),
-    # Circle(mass=1, radius=10 * math.sqrt(1), x=width / 4, y=height / 2, vx=0, vy=100),
-    # *(
-    #     Circle(
-    #         mass=1,
-    #         radius=5 * math.sqrt(1),
-    #         x=width / 2 + width / 4 * math.cos(2 * math.pi * t / 10),
-    #         y=height / 2 + width / 4 * math.sin(2 * math.pi * t / 10),
-    #     )
-    #     for t in range(100)
-    # ),
+    Circle(mass=50, radius=10 * math.sqrt(5), x=width / 2, y=height / 2),
+    Circle(mass=1, radius=10 * math.sqrt(1), x=width / 4, y=height / 2, vx=0, vy=100),
+    *(
+        Circle(
+            mass=1,
+            radius=10 * math.sqrt(1),
+            x=width / 2 + width / 4 * math.cos(2 * math.pi * t / 10),
+            y=height / 2 + width / 4 * math.sin(2 * math.pi * t / 10),
+        )
+        for t in range(3)
+    ),
 ]
 
 
@@ -108,7 +108,7 @@ def update_physics(circle: Circle, timedelta: float, circles: list[Circle]):
 
     # Update velocity based on gravity
     # F_g = G * m1 * m2 / r^2
-    G = 8.0e6  # change this to change gravity
+    G = 8.0e3  # change this to change gravity
     for other_circle in circles:
         if other_circle is not circle:
             # Update velocity based on gravity
